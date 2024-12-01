9a799eab0ae7e9d244c6663a724604212586536f
fer <fernandoatrio@gmail.com>
2024-12-01 15:42:44 +0100

    Add test for the --base-date param

--- calc-date-test.el
+++ calc-date-test.el
@@ -1,5 +1,7 @@
 (load-file "calc-date-lib.el")
 
-(ert-deftest test-add-days ()
+(ert-deftest test-parse-command-line-arguments ()
   (should (equal '(:add-days 3)
-		 (parse-command-line-arguments '("--add-days" "3")))))
+		 (parse-command-line-arguments '("--add-days" "3"))))
+  (should (equal `(:base-date ,(date-to-time "2024-12-25"))
+		 (parse-command-line-arguments '("--base-date" "2024-12-25")))))
