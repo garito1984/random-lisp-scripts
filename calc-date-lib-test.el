3378ba4abfc964572c4312a96835173d997bbfb8
fer <fernandoatrio@gmail.com>
2024-12-06 01:31:31 +0100

    Add some tests

--- calc-date-lib-test.el
+++ calc-date-lib-test.el
@@ -5,3 +5,8 @@
 		 (parse-command-line-arguments '("--add-days" "3"))))
   (should (equal `(:base-date ,(date-to-time "2024-12-25"))
 		 (parse-command-line-arguments '("--base-date" "2024-12-25")))))
+
+(ert-deftest test-days-in-seconds ()
+  (let ((seconds-in-24h (* 60 60 24)))
+    (should (equal (* seconds-in-24h 3)
+		   (days-in-seconds 3)))))
