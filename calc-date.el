70aad8f80f17f8e5e10b9b10266ecdd8cae4ea85
fer <fernandoatrio@gmail.com>
2024-12-08 03:39:11 +0100

    Small refactor

--- calc-date.el
+++ calc-date.el
@@ -2,17 +2,18 @@
 
 (load (concat (file-name-directory load-file-name) "calc-date-lib.el") nil t t)
 
+(defun send-string (string)
+  (send-string-to-terminal string))
+
 (let* ((options (parse-command-line-arguments))
        (days (plist-get options :add-days))
        (base-date (plist-get options :base-date)))
   (when (not days)
     ;; Exit with an error and notify the user about proper usage 
-    (send-string-to-terminal "Usage: calc-date.el --add-days DAYS\n")
-    (send-string-to-terminal "       calc-date.el --base-date 2024-12-25 --add-days DAYS\n")
+    (send-string "Usage: calc-date.el --add-days DAYS\n")
+    (send-string "       calc-date.el --base-date 2024-12-25 --add-days DAYS\n")
     (kill-emacs 1))
 
   ;; The options are correct, start with the processing...
-  (send-string-to-terminal (calc-date-message base-date days))
+  (send-string (calc-date-message base-date days))
   (kill-emacs 0))
-
-
