d0e55c75893ff4d180bc6d82436e24f7cce3da95
fer <fernandoatrio@gmail.com>
2025-08-29 00:17:54 +0200

    Add documentation

--- simple-animation.el
+++ simple-animation.el
@@ -6,21 +6,31 @@
 
     ;; Execute the animation...
     (with-current-buffer-window buff nil nil
+      ;; Add characters to facilitate management
       (self-insert-command 5 ?\ )
+      ;; Set the point to the new location
       (goto-char go-to-char)
-      (setcdr (last chars) chars) ; Make the list circular
+      ;; Make the list circular
+      (setcdr (last chars) chars)
+      ;; Run the animation...
       (animation-progress chars times)
+      ;; Show the animation is finished
       (animation-update ?🗸))))
 
 (defun animation-progress (l n)
   (when (> n 0)
+    ;; Update buffer
     (animation-update (car l))
+    ;; Sleep and redisplay the screen
     (sleep-for 1)
-    (sit-for 0) ; Need this to see the change on screen
+    (sit-for 0)
+    ;; Continue with the animation
     (animation-progress (cdr l) (- n 1))))
 
 (defun animation-update (char)
+  ;; Delete previews animation state
   (delete-backward-char 1)
+  ;; Insert new animation state
   (self-insert-command 1 char))
 
 ;;
